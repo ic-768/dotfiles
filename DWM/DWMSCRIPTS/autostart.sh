f58b49790cef69027381c99969f1c33cb5424758
Ioannis Chrysostomakis <giannis-xri1@hotmail.com>
2020-12-18 23:11:13 +0200

    Status bar tweaks & dunst called on startup

---
+++ DWM/DWMSCRIPTS/autostart.sh
@@ -0,0 +1,7 @@
+sxhkd & disown;
+feh --bg-scale "/home/ic768/Pictures/Wallpapers/japaneseGirl.jpg" & disown;
+/home/ic768/Scripts/DWM/dwmbar & disown;
+/home/ic768/Scripts/Birthdays/birthdayChecker.sh & disown;
+redshift -O 4800 & disown;
+compton & disown;
+dunst & disown;
