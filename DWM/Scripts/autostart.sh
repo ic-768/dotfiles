69847fb1f1f1ddafeb8e797a76fa61bcbe3138d9
Ioannis Chrysostomakis <ic768@protonmail.com>
2021-02-28 16:40:43 +0200

    Auto-start has options for swapping control and alt, mimicing mac keyboard

--- DWM/Scripts/autostart.sh
+++ DWM/Scripts/autostart.sh
@@ -1,7 +1,13 @@
 sxhkd & disown;
-feh --bg-scale "/home/ic768/Pictures/Wallpapers/galaxy.jpg" & disown;
+feh --bg-scale "/home/ic768/Pictures/Wallpapers/japaneseGirl.jpg" & disown;
 /home/ic768/Scripts/DWM/dwmbar & disown;
 /home/ic768/Scripts/Birthdays/birthdayChecker.sh & disown;
 redshift -O 4800 & disown;
 compton & disown;
 dunst & disown;
+
+#Swap alt and control to mimic mac layout
+xmodmap -e "remove mod1 = Alt_L" ;
+xmodmap -e "remove control = Control_L" ;
+xmodmap -e "add control = Alt_L" ;
+xmodmap -e "add mod1 = Control_L";
