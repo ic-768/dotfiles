9f9cd36215c2a90adf615de178240ea3cfe28099
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-02-13 10:52:29 +0200

    ZSH theme; remove ranger and awesome

--- DWM/Scripts/Scripts/Utility/vimgrep.sh
+++ DWM/Scripts/Scripts/Utility/vimgrep.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-vim `grep -r $1 | cut -d ':' -f1`
+# Scan for files containing text and edit them in vim. For regex surround argument with ' '
+#vim `grep -rl $1 | cut -d ':' -f1` 
+vim `grep -rl $1` 
