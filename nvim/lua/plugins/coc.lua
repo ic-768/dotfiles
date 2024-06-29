a4949e7d6ab048e978bdbd477a687193a5bab1ec
ic768 <ic768@protonmail.com>
2024-06-30 00:28:04 +0300

    Fix autocomplete

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -5,18 +5,6 @@ local M = {
 }
 
 M.config = function()
- -- Some servers have issues with backup files, see #649
-vim.opt.backup = false
-vim.opt.writebackup = false
-
--- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
--- delays and poor user experience
-vim.opt.updatetime = 300
-
--- Always show the signcolumn, otherwise it would shift the text each time
--- diagnostics appeared/became resolved
-vim.opt.signcolumn = "yes"
-
 local keyset = vim.keymap.set
 
 keyset('n', 'gd', '<Plug>(coc-definition)', { silent = true })
@@ -40,17 +28,19 @@ function _G.show_docs()
 end
 
 keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+keyset('n', 'K', ':lua vim.fn.CocActionAsync("doHover")<CR>', { silent = true })
+keyset('n', ':E', '<Cmd>CocCommand explorer<CR>', { silent = true })
 
-vim.api.nvim_set_keymap('n', 'K', ':lua vim.fn.CocActionAsync("doHover")<CR>', { silent = true })
-vim.keymap.set('i', '<C-k>', 'coc#refresh()', { silent = true, expr = true })
-vim.api.nvim_set_keymap('n', ':E', '<Cmd>CocCommand explorer<CR>', { silent = true })
-
--- TODO this isn't working
-vim.api.nvim_set_keymap('n', '<silent> <C-k>', ':viwo<CR>:normal ea<C-k><CR>', {silent=true})
+keyset('i', '<C-k>', 'coc#refresh()', { silent = true, expr = true })
+vim.keymap.set("n", "<C-k>", function()
+  vim.api.nvim_feedkeys("viwoeA","n", false)
+  vim.fn["coc#refresh"]()
 end
+)
 
-vim.api.nvim_set_keymap('x', '<leader>a', '<Plug>(coc-codeaction-selected)', {})
-vim.api.nvim_set_keymap('n', '<leader>a', '<Plug>(coc-codeaction-selected)', {})
+keyset('x', '<leader>a', '<Plug>(coc-codeaction-selected)', {})
+keyset('n', '<leader>a', '<Plug>(coc-codeaction-selected)', {})
 
+end
 
 return M
